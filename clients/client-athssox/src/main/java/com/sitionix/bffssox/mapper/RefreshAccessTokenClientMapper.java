384b597e2487ec794d2b83ab54bab5110e870dc9
vladvinskevitch <vvinskevitch@gmail.com>
2026-02-06 13:57:29 +0200

    [SITIONIX-68] - add resend endpoint

--- clients/client-athssox/src/main/java/com/sitionix/bffssox/mapper/RefreshAccessTokenClientMapper.java
+++ clients/client-athssox/src/main/java/com/sitionix/bffssox/mapper/RefreshAccessTokenClientMapper.java
@@ -5,11 +5,13 @@ import com.app_afesox.athssox.client.dto.RefreshAccessTokenResponseDTO;
 import com.sitionix.bffssox.domain.RefreshAccessTokenRequest;
 import com.sitionix.bffssox.domain.RefreshAccessTokenResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RefreshAccessTokenClientMapper {
 
     RefreshAccessTokenRequestDTO asRefreshAccessTokenRequestDto(RefreshAccessTokenRequest src);
 
+    @Mapping(target = "tokenType", expression = "java(src.getTokenType().getValue())")
     RefreshAccessTokenResponse asRefreshAccessTokenResponse(RefreshAccessTokenResponseDTO src);
 }
