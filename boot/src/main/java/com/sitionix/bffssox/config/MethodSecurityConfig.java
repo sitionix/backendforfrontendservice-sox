2da049b44cebbbb0802c82260644aaef38fa8faa
vladvinskevitch <vvinskevitch@gmail.com>
2026-02-18 09:20:50 +0200

    [SITIONIX-76] - remove JacksonConfig file and use primary ObjectMapper bean

--- boot/src/main/java/com/sitionix/bffssox/config/MethodSecurityConfig.java
+++ boot/src/main/java/com/sitionix/bffssox/config/MethodSecurityConfig.java
@@ -1,9 +1,23 @@
 package com.sitionix.bffssox.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableMethodSecurity
 public class MethodSecurityConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
 }
