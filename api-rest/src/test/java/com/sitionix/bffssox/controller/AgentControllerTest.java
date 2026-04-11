0df65240787a1de90a54d2d9af7479270182b72a
vladvinskevitch <vvinskevitch@gmail.com>
2026-04-11 09:38:08 +0300

    [SITIONIX-105] - add agent bff unit tests

---
+++ api-rest/src/test/java/com/sitionix/bffssox/controller/AgentControllerTest.java
@@ -0,0 +1,130 @@
+package com.sitionix.bffssox.controller;
+
+import com.app_afesox.bffssox.api_first.dto.AgentDTO;
+import com.app_afesox.bffssox.api_first.dto.AgentsResponseDTO;
+import com.app_afesox.bffssox.api_first.dto.CreateAgentRequestDTO;
+import com.sitionix.bffssox.domain.Agent;
+import com.sitionix.bffssox.domain.AgentsResponse;
+import com.sitionix.bffssox.domain.CreateAgentRequest;
+import com.sitionix.bffssox.mapper.AgentApiMapper;
+import com.sitionix.bffssox.mapper.CreateAgentApiMapper;
+import com.sitionix.bffssox.usecase.CreateAgent;
+import com.sitionix.bffssox.usecase.GetAgent;
+import com.sitionix.bffssox.usecase.GetAgents;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AgentControllerTest {
+
+    private AgentController agentController;
+
+    @Mock
+    private CreateAgentApiMapper createAgentApiMapper;
+
+    @Mock
+    private AgentApiMapper agentApiMapper;
+
+    @Mock
+    private CreateAgent createAgent;
+
+    @Mock
+    private GetAgents getAgents;
+
+    @Mock
+    private GetAgent getAgent;
+
+    @BeforeEach
+    void setUp() {
+        this.agentController = new AgentController(
+                this.createAgentApiMapper,
+                this.agentApiMapper,
+                this.createAgent,
+                this.getAgents,
+                this.getAgent
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(
+                this.createAgentApiMapper,
+                this.agentApiMapper,
+                this.createAgent,
+                this.getAgents,
+                this.getAgent
+        );
+    }
+
+    @Test
+    void givenCreateAgentRequestDto_whenCreateAgent_thenReturnCreatedResponse() {
+        //given
+        final CreateAgentRequestDTO requestDTO = mock(CreateAgentRequestDTO.class);
+        final CreateAgentRequest request = mock(CreateAgentRequest.class);
+        final Agent response = mock(Agent.class);
+        final AgentDTO responseDTO = mock(AgentDTO.class);
+
+        when(this.createAgentApiMapper.asCreateAgentRequest(requestDTO)).thenReturn(request);
+        when(this.createAgent.execute(request)).thenReturn(response);
+        when(this.agentApiMapper.asAgentDto(response)).thenReturn(responseDTO);
+
+        //when
+        final ResponseEntity<AgentDTO> actual = this.agentController.createAgent(requestDTO);
+
+        //then
+        assertThat(actual).isEqualTo(ResponseEntity.status(HttpStatus.CREATED).body(responseDTO));
+        verify(this.createAgentApiMapper).asCreateAgentRequest(requestDTO);
+        verify(this.createAgent).execute(request);
+        verify(this.agentApiMapper).asAgentDto(response);
+    }
+
+    @Test
+    void givenGetAgentsRequest_whenGetAgents_thenReturnOkResponse() {
+        //given
+        final AgentsResponse response = mock(AgentsResponse.class);
+        final AgentsResponseDTO responseDTO = mock(AgentsResponseDTO.class);
+
+        when(this.getAgents.execute()).thenReturn(response);
+        when(this.agentApiMapper.asAgentsResponseDto(response)).thenReturn(responseDTO);
+
+        //when
+        final ResponseEntity<AgentsResponseDTO> actual = this.agentController.getAgents();
+
+        //then
+        assertThat(actual).isEqualTo(ResponseEntity.ok(responseDTO));
+        verify(this.getAgents).execute();
+        verify(this.agentApiMapper).asAgentsResponseDto(response);
+    }
+
+    @Test
+    void givenAgentId_whenGetAgent_thenReturnOkResponse() {
+        //given
+        final UUID agentId = UUID.fromString("ebac37f0-90a2-4f6b-ab99-73f6ac5cf675");
+        final Agent response = mock(Agent.class);
+        final AgentDTO responseDTO = mock(AgentDTO.class);
+
+        when(this.getAgent.execute(agentId)).thenReturn(response);
+        when(this.agentApiMapper.asAgentDto(response)).thenReturn(responseDTO);
+
+        //when
+        final ResponseEntity<AgentDTO> actual = this.agentController.getAgent(agentId);
+
+        //then
+        assertThat(actual).isEqualTo(ResponseEntity.ok(responseDTO));
+        verify(this.getAgent).execute(agentId);
+        verify(this.agentApiMapper).asAgentDto(response);
+    }
+}
---
+++ application/src/test/java/com/sitionix/bffssox/usecase/GetAgentsImplTest.java
@@ -0,0 +1,49 @@
+package com.sitionix.bffssox.usecase;
+
+import com.sitionix.bffssox.client.AgentClient;
+import com.sitionix.bffssox.domain.AgentsResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetAgentsImplTest {
+
+    private GetAgents getAgents;
+
+    @Mock
+    private AgentClient agentClient;
+
+    @BeforeEach
+    void setUp() {
+        this.getAgents = new GetAgentsImpl(this.agentClient);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(this.agentClient);
+    }
+
+    @Test
+    void givenGetAgentsRequest_whenExecute_thenReturnAgentsResponse() {
+        //given
+        final AgentsResponse response = mock(AgentsResponse.class);
+        when(this.agentClient.getAgents()).thenReturn(response);
+
+        //when
+        final AgentsResponse actual = this.getAgents.execute();
+
+        //then
+        assertThat(actual).isEqualTo(response);
+        verify(this.agentClient).getAgents();
+    }
+}
