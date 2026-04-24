c03c34e9d89012572f9b26c90e26ea85e742c910
vladvinskevitch <vvinskevitch@gmail.com>
2026-04-24 15:11:56 +0300

    [SITIONIX-119] - cover accept and reject agent rule use cases

---
+++ application/src/test/java/com/sitionix/bffssox/usecase/AcceptAgentRuleImplTest.java
@@ -0,0 +1,56 @@
+package com.sitionix.bffssox.usecase;
+
+import com.sitionix.bffssox.client.AgentClient;
+import com.sitionix.bffssox.domain.AcceptAgentRuleRequest;
+import com.sitionix.bffssox.domain.AgentRule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AcceptAgentRuleImplTest {
+
+    private AcceptAgentRuleImpl acceptAgentRule;
+
+    @Mock
+    private AgentClient agentClient;
+
+    @BeforeEach
+    void setUp() {
+        this.acceptAgentRule = new AcceptAgentRuleImpl(this.agentClient);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(this.agentClient);
+    }
+
+    @Test
+    void givenAgentIdAndRuleIdAndRequest_whenExecute_thenDelegateToClient() {
+        //given
+        final UUID agentId = UUID.fromString("4f7cbfcb-1d84-4463-ad97-a8387d4eaaf6");
+        final UUID ruleId = UUID.fromString("89bb4ed2-4c9b-412d-8a68-f4c6f6ec489a");
+        final AcceptAgentRuleRequest request = mock(AcceptAgentRuleRequest.class);
+        final AgentRule expected = mock(AgentRule.class);
+
+        when(this.agentClient.acceptAgentRule(agentId, ruleId, request)).thenReturn(expected);
+
+        //when
+        final AgentRule actual = this.acceptAgentRule.execute(agentId, ruleId, request);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+        verify(this.agentClient).acceptAgentRule(agentId, ruleId, request);
+    }
+}
---
+++ application/src/test/java/com/sitionix/bffssox/usecase/RejectAgentRuleImplTest.java
@@ -0,0 +1,54 @@
+package com.sitionix.bffssox.usecase;
+
+import com.sitionix.bffssox.client.AgentClient;
+import com.sitionix.bffssox.domain.AgentRule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RejectAgentRuleImplTest {
+
+    private RejectAgentRuleImpl rejectAgentRule;
+
+    @Mock
+    private AgentClient agentClient;
+
+    @BeforeEach
+    void setUp() {
+        this.rejectAgentRule = new RejectAgentRuleImpl(this.agentClient);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(this.agentClient);
+    }
+
+    @Test
+    void givenAgentIdAndRuleId_whenExecute_thenDelegateToClient() {
+        //given
+        final UUID agentId = UUID.fromString("4f7cbfcb-1d84-4463-ad97-a8387d4eaaf6");
+        final UUID ruleId = UUID.fromString("89bb4ed2-4c9b-412d-8a68-f4c6f6ec489a");
+        final AgentRule expected = mock(AgentRule.class);
+
+        when(this.agentClient.rejectAgentRule(agentId, ruleId)).thenReturn(expected);
+
+        //when
+        final AgentRule actual = this.rejectAgentRule.execute(agentId, ruleId);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+        verify(this.agentClient).rejectAgentRule(agentId, ruleId);
+    }
+}
